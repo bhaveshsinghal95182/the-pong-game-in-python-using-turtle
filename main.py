7f618db59ec54a660e620163163c0fd6007e2c9f
bhaveshsinghal95182 <168349039+bhaveshsinghal95182@users.noreply.github.com>
2024-05-11 15:43:43 +0530

    creating both paddles and giving them functionality

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-from turtle import Screen
+from turtle import Screen, Turtle
+from paddle import Paddle
 
 screen = Screen()
 screen.screensize(800,600,"black")
 screen.title("pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350,0))
+l_paddle = Paddle((-350,0))
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_on = True
+while game_on:
+    screen.update()
 
 screen.exitonclick()
